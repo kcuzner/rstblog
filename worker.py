e1d86d11428151a0cf782571ea77070ee15cb622
Kevin Cuzner <kevin@kevincuzner.com>
2021-11-28 21:11:27 -0600

    Adding post rendering

--- worker.py
+++ worker.py
@@ -51,8 +51,6 @@ class PygmentsDirective(rst.Directive):
 
         pygments_formatter = HtmlFormatter()
 
-        print(f"Rendering {self.content}")
-
         try:
             lexer = get_lexer_by_name(self.arguments[0])
         except ValueError:
@@ -86,7 +84,12 @@ class RstBlogSettingsDirective(rst.Directive):
 
         if argument is None:
             raise ValueError("argument required but not supplied")
-        return datetime.strptime(argument, "%Y/%m/%d")
+        for fmt in ("%Y/%m/%d", "%d %B %Y", "%d %b %Y"):
+            try:
+                return datetime.strptime(argument, fmt)
+            except ValueError:
+                pass
+        raise ValueError(f"Unrecognized date format: {argument}")
 
     required_arguments = 0
     optional_arguments = 0
@@ -162,6 +165,71 @@ class BlogWriter(html4css1.Writer):
                 self.parts[part] = "".join(getattr(self, part))
 
 
+class Renderer:
+    def __init__(self, src):
+        self.src = src
+
+    def render(self, jinja_env, out_dir):
+        import shutil
+        from pathlib import Path
+        import docutils.core
+
+        print(f"Rendering {self.src}")
+        # Parse the document
+        with open(self.src) as f:
+            parts = docutils.core.publish_parts(source=f.read(), writer=BlogWriter())
+        doc_settings = parts["rstblog_settings"]
+        doc_content = parts["rstblog_content"]
+        title = doc_settings["title"]
+        # Determine page path
+        doc_dir = self.get_dir(out_dir, doc_settings)
+        out_path = os.path.join(doc_dir, "index.html")
+        os.makedirs(doc_dir, exist_ok=True)  # Subpaths of dates may exist
+        # Copy content
+        print(f"Copying {len(doc_content)} items into {doc_dir}")
+        for item in doc_content:
+            item_src = os.path.join(Path(self.src).parent, item)
+            item_dest = os.path.join(doc_dir, item)
+            shutil.copyfile(item_src, item_dest)
+        print(f"Rendering into {out_path}")
+        template = self.get_template(jinja_env)
+        with open(out_path, "w") as f:
+            f.write(template.render(parts=parts))
+
+    def get_template(self, jinja_env):
+        raise NotImplementedError(f"get_template must be overriden in {type(self)}")
+
+    def get_dir(self, out_dir, page_settings):
+        raise NotImplementedError(f"get_template must be overridden in {type(self)}")
+
+
+class PageRenderer(Renderer):
+    def get_template(self, jinja_env):
+        return jinja_env.get_template(settings["blog"]["page"])
+
+    def get_dir(self, out_dir, page_settings):
+        url = page_settings["url"]
+        if not os.path.isabs(url):
+            raise ValueError(
+                f'Document {self.src} needs an absolute path, "{page_settings["url"]}" supplied'
+            )
+        return os.path.join(out_dir, os.path.relpath(url, "/"))
+
+
+class PostRenderer(Renderer):
+    def get_template(self, jinja_env):
+        return jinja_env.get_template(settings["blog"]["post"])
+
+    def get_dir(self, out_dir, page_settings):
+        url = page_settings["url"]
+        date = page_settings["date"].strftime("%Y/%m/%d")  # YYY/MM/DD
+        if not os.path.isabs(url):
+            raise ValueError(
+                f'Document {self.src} needs an absolute path, "{page_settings["url"]}" supplied'
+            )
+        return os.path.join(out_dir, date, os.path.relpath(url, "/"))
+
+
 @app.task
 def clone_repository():
     """
@@ -170,7 +238,6 @@ def clone_repository():
     repo_url = settings["repository"]["url"]
     repo_dir = os.path.abspath(settings["repository"]["directory"])
     print(f"Cloning {repo_url} into {repo_dir}")
-    os.makedirs(repo_dir, exist_ok=True)
     subprocess.run(["git", "clone", repo_url, repo_dir])
 
 
@@ -180,8 +247,6 @@ def update():
     Updates the repo and re-renders all content
     """
     import glob, shutil
-    from pathlib import Path
-    import docutils.core
     from jinja2 import Environment, FileSystemLoader, select_autoescape
 
     repo_dir = os.path.abspath(settings["repository"]["directory"])
@@ -215,35 +280,11 @@ def update():
             if os.path.exists(s[1]):
                 shutil.copytree(s[1], os.path.abspath(s[0]))
         # Render each page
-        page_tmpl = jinja_env.get_template(settings["blog"]["page"])
         for page in pages:
-            print(f"Rendering {page}")
-            # Parse the page
-            with open(page) as f:
-                parts = docutils.core.publish_parts(
-                    source=f.read(), writer=BlogWriter()
-                )
-            page_settings = parts["rstblog_settings"]
-            page_content = parts["rstblog_content"]
-            title = page_settings["title"]
-            url = page_settings["url"]
-            # Determine page path
-            if not os.path.isabs(url):
-                raise ValueError(
-                    f'Page {page} needs an absolute path, "{page_settings["url"]}" supplied'
-                )
-            page_dir = os.path.join(out_dir, os.path.relpath(url, "/"))
-            os.makedirs(page_dir)
-            out_path = os.path.join(page_dir, "index.html")
-            # Copy content
-            print(f"Copying {len(page_content)} items into {out_dir}")
-            for item in page_content:
-                item_src = os.path.join(Path(page).parent, item)
-                item_dest = os.path.join(page_dir, item)
-                shutil.copyfile(item_src, item_dest)
-            print(f"Rendering into {out_path}")
-            with open(out_path, "w") as f:
-                f.write(page_tmpl.render(parts=parts))
+            PageRenderer(page).render(jinja_env, out_dir)
+        # Render each post
+        for post in posts:
+            PostRenderer(post).render(jinja_env, out_dir)
         # Render the index
         index = jinja_env.get_template(settings["blog"]["index"])
         with open("./index.html", "w") as f:
