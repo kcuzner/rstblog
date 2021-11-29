d4a2c37b78cb606ad598aa1ad413db7b4ec3bf97
Kevin Cuzner <kevin@kevincuzner.com>
2021-11-28 20:31:51 -0600

    Adding page rendering using rstblog-settings

--- worker.py
+++ worker.py
@@ -2,7 +2,9 @@ from celery import Celery
 
 import subprocess, os
 
+import docutils.nodes
 from docutils.parsers import rst
+from docutils.writers import html4css1
 import toml
 
 settings = toml.load("./settings.toml")
@@ -43,7 +45,6 @@ class PygmentsDirective(rst.Directive):
     has_content = True
 
     def run(self):
-        from docutils import nodes
         from pygments import highlight
         from pygments.lexers import get_lexer_by_name
         from pygments.formatters import HtmlFormatter
@@ -58,10 +59,107 @@ class PygmentsDirective(rst.Directive):
             # no lexer found - use the text one instead of an exception
             lexer = get_lexer_by_name("text")
         parsed = highlight("\n".join(self.content), lexer, pygments_formatter)
-        return [nodes.raw("", parsed, format="html")]
+        return [docutils.nodes.raw("", parsed, format="html")]
 
 
 rst.directives.register_directive("code-block", PygmentsDirective)
+
+
+class rstblog_settings(docutils.nodes.Element):
+    pass
+
+
+class RstBlogSettingsDirective(rst.Directive):
+    """
+    Handle rstblog-settings directives
+
+    Date must be in the format YYYY/MM/DD
+    """
+
+    def taglist(argument):
+        if argument and argument.strip():
+            return [x.strip() for x in argument.split(",") if x.strip()]
+        return []
+
+    def date(argument):
+        from datetime import datetime
+
+        if argument is None:
+            raise ValueError("argument required but not supplied")
+        return datetime.strptime(argument, "%Y/%m/%d")
+
+    required_arguments = 0
+    optional_arguments = 0
+    option_spec = {
+        "title": rst.directives.unchanged_required,
+        "date": date,
+        "url": rst.directives.uri,
+        "tags": taglist,
+    }
+
+    def run(self):
+        node = rstblog_settings()
+        for name in self.option_spec:
+            if name in self.options:
+                node[name] = self.options[name]
+            else:
+                node[name] = self.option_spec[name](None)
+        return [node]
+
+
+rst.directives.register_directive("rstblog-settings", RstBlogSettingsDirective)
+
+
+class BlogTranslator(html4css1.HTMLTranslator):
+    """
+    Customized docutils translator which handles the rstblog-settings nodes
+    declaring metadata about a document.
+    """
+
+    def __init__(self, document):
+        super().__init__(document)
+        self.rstblog_settings = {}
+        self.rstblog_content = []
+
+    def visit_image(self, node):
+        super().visit_image(node)
+        # Collect any images referenced
+        uri = node["uri"]
+        if os.path.isabs(uri):
+            raise ValueError(
+                f"Image {node} references an absolute path to an image, this is not allowed"
+            )
+        self.rstblog_content.append(uri)
+
+    def visit_rstblog_settings(self, node):
+        for setting, value in node.attlist():
+            self.rstblog_settings[setting] = value
+
+    def depart_rstblog_settings(self, node):
+        pass
+
+
+class BlogWriter(html4css1.Writer):
+    """
+    Customized docutils writer which handles the rstblog-settings nodes
+    declaring metadata about a document.
+    """
+
+    rstblog_attributes = ("rstblog_settings", "rstblog_content")
+
+    visitor_attributes = html4css1.Writer.visitor_attributes + rstblog_attributes
+
+    def __init__(self):
+        super().__init__()
+        self.translator_class = BlogTranslator
+
+    def assemble_parts(self):
+        super().assemble_parts()
+        for part in self.visitor_attributes:
+            if part in self.rstblog_attributes:
+                self.parts[part] = getattr(self, part)
+            else:
+                self.parts[part] = "".join(getattr(self, part))
 
 
 @app.task
@@ -117,18 +215,36 @@ def update():
             if os.path.exists(s[1]):
                 shutil.copytree(s[1], os.path.abspath(s[0]))
         # Render each page
+        page_tmpl = jinja_env.get_template(settings["blog"]["page"])
         for page in pages:
-            out_path = Path(page).stem + ".html"
-            print(f"Rendering {page} into {out_path}")
+            print(f"Rendering {page}")
+            # Parse the page
             with open(page) as f:
-                parts = docutils.core.publish_parts(source=f.read(), writer_name="html")
-            page_tmpl = jinja_env.get_template(settings["blog"]["page"])
+                parts = docutils.core.publish_parts(
+                    source=f.read(), writer=BlogWriter()
+                )
+            page_settings = parts["rstblog_settings"]
+            page_content = parts["rstblog_content"]
+            title = page_settings["title"]
+            url = page_settings["url"]
+            # Determine page path
+            if not os.path.isabs(url):
+                raise ValueError(
+                    f'Page {page} needs an absolute path, "{page_settings["url"]}" supplied'
+                )
+            page_dir = os.path.join(out_dir, os.path.relpath(url, "/"))
+            os.makedirs(page_dir)
+            out_path = os.path.join(page_dir, "index.html")
+            # Copy content
+            print(f"Copying {len(page_content)} items into {out_dir}")
+            for item in page_content:
+                item_src = os.path.join(Path(page).parent, item)
+                item_dest = os.path.join(page_dir, item)
+                shutil.copyfile(item_src, item_dest)
+            print(f"Rendering into {out_path}")
             with open(out_path, "w") as f:
                 f.write(page_tmpl.render(parts=parts))
         # Render the index
         index = jinja_env.get_template(settings["blog"]["index"])
         with open("./index.html", "w") as f:
             f.write(index.render())
-        # docutils.core.publish_file(
-        #     source_path=index, destination_path="./index.html", writer_name="html"
-        # )
