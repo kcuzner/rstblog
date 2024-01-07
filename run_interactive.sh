4ac1b24394c440d57effd5e359a4642c4ad6f90a
Kevin Cuzner <kevin@kevincuzner.com>
2024-01-07 15:08:41 -0600

    Changing over to gunicorn and setting things up for production

--- run_interactive.sh
+++ run_interactive.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker-compose -f docker-compose.yml -f docker-compose.override.yml build
-docker-compose -f docker-compose.yml -f docker-compose.override.yml up
+docker compose -f docker-compose.yml -f docker-compose.override.yml build
+docker compose -f docker-compose.yml -f docker-compose.override.yml up
