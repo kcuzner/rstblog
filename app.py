932b128c5fca475aad738326e3fecdea2c8a1c66
Kevin Cuzner <kevin@kevincuzner.com>
2024-01-14 22:17:02 +0000

    Adding HMAC validation to rstblog

--- app.py
+++ app.py
@@ -2,36 +2,44 @@
 
 from flask import Flask, request
 
-import asyncio, atexit, datetime, enum, queue, subprocess, os
-from functools import wraps
+import functools
+import hashlib
+import hmac
+import logging
 import toml
 
 import worker
 
 settings = toml.load("./settings.toml")
-actions = queue.Queue()
 app = Flask(__name__)
 
 worker.clone_repository.delay().wait()
 
+_log = app.logger
 
 def validate_hmac(f):
-    @wraps(f)
+    @functools.wraps(f)
     def wrapper(*args, **kwargs):
-        return {}, 401
+        sig = request.headers.get("X-Hub-Signature-256", None)
+        if sig is None:
+            return {"message": "X-Hub-Signature-256 header missing"}, 403
+        hashobj = hmac.new(
+            settings["server"]["secret"].encode("utf-8"),
+            msg=request.data,
+            digestmod=hashlib.sha256,
+        )
+        expected = "sha256=" + hashobj.hexdigest()
+        if not hmac.compare_digest(expected, sig):
+            return {"message": "HMAC validation failed"}, 401
+        _log.debug("Validated HMAC")
+        return f(*args, **kwargs)
 
     return wrapper
 
 
-@app.route("/test")
-def hi():
-    worker.update.delay()
-    return {}
-
-
 @app.route("/refresh", methods=["POST"])
 @validate_hmac
-async def request_refresh():
-    data = request.json
-    print(data)
+def request_refresh():
+    _log.info("Refresh request received")
+    worker.update.delay()
     return {}
